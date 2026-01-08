163fa2685dcb3cf30b7deba664f96da0e4b029fa
Anna Gladush <ann.gladush@gmail.com>
2026-01-08 16:22:21 +0200

    Finished nested arrays. Last task was very tough

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -2,7 +2,7 @@ def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
-
+  Array.new(number_of_rows) {Array.new(seats_per_row)}
   # Example: blank_seating_chart(2, 3) should return:
   # [
   #   [nil, nil, nil],
@@ -14,30 +14,41 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
-  # take a chart (2d array)  and add seat_to_add to the end of the row that is
-  # at row_index index of the chart, then return the chart
+  # take a chart (2d array) and add seat_to_add to the end of the row that is at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  chart
 end
 
 def add_another_row(chart, row_to_add)
-  # take a chart and add row_to_add to the end of the chart,
-  # then return the chart.
+  # take a chart and add row_to_add to the end of the chart, then return the chart.
+  chart << row_to_add
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
-  # take a chart and delete the seat at seat_index of the row at row_index of
-  # the chart, then return the chart
-
+  # take a chart and delete the seat at seat_index of the row at row_index of the chart, then return the chart
+  chart[row_index].delete_at(seat_index)
+  chart
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
 
 def delete_row_from_chart(chart, row_index)
-  # take a chart and delete the row at row_index of the chart,
-  # then return the chart
+  # take a chart and delete the row at row_index of the chart, then return the chart
+  chart.delete_at(row_index)
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
-
+  result = 0
+  chart.each_with_index do |row, row_idx|
+    row.each_with_index do |seat, col_idx|
+      if seat == nil
+        result += 1
+      end
+    end
+  end
+  result
   # NOTE: `chart` should **not** be mutated
 end
 
@@ -53,6 +64,12 @@ def find_favorite(array_of_hash_objects)
   #   { name: 'HTML', is_my_favorite?: false }
   # ]
 
+  answer = array_of_hash_objects.each do |hash|
+    hash.each do |key, value| 
+      return Hash[hash.to_a.reverse] if key == :is_my_favorite? && value == true
+      end 
+    end
+    return nil if answer.respond_to?(:to_h)
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
 end
